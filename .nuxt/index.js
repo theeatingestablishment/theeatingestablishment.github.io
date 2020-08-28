2b17efb006e6969cc62703d3b2ca77ac23000a95
Nino Aguilar <ninoaguilar95@gmail.com>
2020-08-28 15:12:48 -0600

    packages update

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+
 import Meta from 'vue-meta'
 import ClientOnly from 'vue-client-only'
 import NoSsr from 'vue-no-ssr'
@@ -184,9 +185,9 @@ async function createApp(ssrContext, config = {}) {
   if (process.server && ssrContext && ssrContext.url) {
     await new Promise((resolve, reject) => {
       router.push(ssrContext.url, resolve, (err) => {
-        // https://github.com/vuejs/vue-router/blob/v3.3.4/src/history/errors.js
+        // https://github.com/vuejs/vue-router/blob/v3.4.3/src/util/errors.js
         if (!err._isRouter) return reject(err)
-        if (err.type !== 1 /* NavigationFailureType.redirected */) return resolve()
+        if (err.type !== 2 /* NavigationFailureType.redirected */) return resolve()
 
         // navigated to a different route in router guard
         const unregister = router.afterEach(async (to, from) => {
